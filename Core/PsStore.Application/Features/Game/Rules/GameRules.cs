fb82b2204bd65eaa384100d50a926f2fc406c7e6
tair800 <146845940+tair800@users.noreply.github.com>
2025-03-08 03:24:40 +0100

    Reapply "game create"
    This reverts commit 8ca9b82465cf8a465987a0a82c48a6a9f8477e8b.

---
+++ Core/PsStore.Application/Features/Game/Rules/GameRules.cs
@@ -0,0 +1,16 @@
+﻿using PsStore.Application.Bases;
+using PsStore.Application.Features.Game.Exceptions;
+
+namespace PsStore.Application.Features.Game.Rules
+{
+    public class GameRules : BaseRules
+    {
+        public Task GameTitleMustNotBeSame(IList<Domain.Entities.Game> games, string requestTitle)
+        {
+            if (games.Any(x => x.Title == requestTitle))
+                throw new GameTitleMustNotBeSameException();
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ Core/PsStore.Domain/Entities/Promo.cs
@@ -0,0 +1,10 @@
+﻿using PsStore.Domain.Common;
+
+namespace PsStore.Domain.Entities
+{
+    public class Promo : EntityBase
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+    }
+}
