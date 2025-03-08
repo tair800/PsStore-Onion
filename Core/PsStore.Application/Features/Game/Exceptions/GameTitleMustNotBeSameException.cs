b63e37856cd50bdaa3ede50b49a23c17bc7f0d9e
tair800 <146845940+tair800@users.noreply.github.com>
2025-03-08 03:24:40 +0100

    Reapply "game create"
    This reverts commit 8ca9b82465cf8a465987a0a82c48a6a9f8477e8b.

---
+++ Core/PsStore.Application/Features/Game/Exceptions/GameTitleMustNotBeSameException.cs
@@ -0,0 +1,8 @@
+﻿namespace PsStore.Application.Features.Game.Exceptions
+{
+    public class GameTitleMustNotBeSameException : Exception
+    {
+        public GameTitleMustNotBeSameException()
+            : base("A game with the same title already exists.") { }
+    }
+}
---
+++ Core/PsStore.Domain/Entities/Setting.cs
@@ -0,0 +1,10 @@
+﻿using PsStore.Domain.Common;
+
+namespace PsStore.Domain.Entities
+{
+    public class Setting : EntityBase
+    {
+        public string Key { get; set; }
+        public string Value { get; set; }
+    }
+}
