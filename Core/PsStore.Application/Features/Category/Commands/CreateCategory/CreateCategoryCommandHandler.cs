17f38f893b226c335c8f62b9f4bfd79db8870b86
tair800 <146845940+tair800@users.noreply.github.com>
2025-03-10 04:09:16 +0100

    category

---
+++ Core/PsStore.Application/Features/Category/Commands/CreateCategory/CreateCategoryCommandHandler.cs
@@ -0,0 +1,42 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Http;
+using PsStore.Application.Bases;
+using PsStore.Application.Interfaces.AutoMapper;
+using PsStore.Application.Interfaces.UnitOfWorks;
+
+namespace PsStore.Application.Features.Category.Commands
+{
+    public class CreateCategoryCommandHandler : BaseHandler, IRequestHandler<CreateCategoryCommandRequest, Unit>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CreateCategoryCommandHandler(IMapper mapper, IUnitOfWork unitOfWork, IHttpContextAccessor httpContextAccessor)
+            : base(mapper, unitOfWork, httpContextAccessor)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Unit> Handle(CreateCategoryCommandRequest request, CancellationToken cancellationToken)
+        {
+            // Check if category already exists
+            var existingCategory = await _unitOfWork.GetReadRepository<Domain.Entities.Category>()
+       .GetAsync(c => c.Name.Trim().ToLower() == request.Name.Trim().ToLower());
+
+            if (existingCategory != null)
+            {
+                throw new Exception("Category with the same name already exists.");
+            }
+
+            // Create new category entity
+            Domain.Entities.Category category = new(request.Name);
+
+            // Add the category to the repository
+            await _unitOfWork.GetWriteRepository<Domain.Entities.Category>().AddAsync(category);
+
+            // Save changes
+            await _unitOfWork.SaveAsync();
+
+            return Unit.Value;
+        }
+    }
+}
---
+++ Core/PsStore.Application/Features/Category/Commands/CreateCategory/CreateCategoryCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace PsStore.Application.Features.Category.Commands
+{
+    public class CreateCategoryCommandValidator : AbstractValidator<CreateCategoryCommandRequest>
+    {
+        //public CreateCategoryCommandValidator()
+        //{
+        //    RuleFor(x => x.Name)
+        //        .NotEmpty().WithMessage("Category name is required.")
+        //        .MaximumLength(100).WithMessage("Category name cannot exceed 100 characters.");
+        //}
+    }
+}
